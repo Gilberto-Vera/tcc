84c63c8777543bcf26ceafaa637f69f9cb68c435
Gilberto-Vera <rasvejr@gmail.com>
2021-07-28 21:45:57 -0300

    otimizar sidebar

--- view/sidebar/sidebar.php
+++ view/sidebar/sidebar.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,48 +11,43 @@
   </head>
   <body>
 
-<main>
-<div class="flex-shrink-0 p-3 bg-black" style="width: 280px;">
-    <a href="#" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-      <img class="mb-4" src="image/ring.png" alt="" width="50" height="72">
-      <span class="fs-5 fw-semibold">SGCC</span>
+  <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+    <a href="#" class="d-flex align-items-center justify-content-md-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      <img class="mb-4" src="image/ring.png" width="50" height="72"><br>
+      <!-- <span class="fs-5 fw-semibold">SGCC</span> -->
     </a>
     <ul class="list-unstyled ps-0">
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
-          Home
+          Cliente
         </button>
         <div class="collapse" id="home-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Updates</a></li>
-            <li><a href="#" class="link-dark rounded">Reports</a></li>
+            <li><a href="#" class="link-dark rounded">Cadastro</a></li>
+            <li><a href="#" class="link-dark rounded">Lista</a></li>
           </ul>
         </div>
       </li>
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-          Dashboard
+          Evento
         </button>
         <div class="collapse" id="dashboard-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Weekly</a></li>
-            <li><a href="#" class="link-dark rounded">Monthly</a></li>
-            <li><a href="#" class="link-dark rounded">Annually</a></li>
+            <li><a href="#" class="link-dark rounded">Cadastro</a></li>
+            <li><a href="#" class="link-dark rounded">Lista</a></li>
+            <li><a href="#" class="link-dark rounded">Convidados</a></li>
           </ul>
         </div>
       </li>
       <li class="mb-1">
         <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-          Orders
+          Fornecedor
         </button>
         <div class="collapse" id="orders-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">New</a></li>
-            <li><a href="#" class="link-dark rounded">Processed</a></li>
-            <li><a href="#" class="link-dark rounded">Shipped</a></li>
-            <li><a href="#" class="link-dark rounded">Returned</a></li>
+            <li><a href="#" class="link-dark rounded">Cadastro</a></li>
+            <li><a href="#" class="link-dark rounded">Lista</a></li>
           </ul>
         </div>
       </li>
@@ -62,8 +58,8 @@
         </button>
         <div class="collapse" id="account-collapse">
           <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Novo...</a></li>
-            <li><a href="#" class="link-dark rounded">Conta</a></li>
+            <li><a href="#" class="link-dark rounded">Cadastro</a></li>
+            <li><a href="#" class="link-dark rounded">Perfil</a></li>
             <li><a href="#" class="link-dark rounded">Configurações</a></li>
             <li><a href="index.php?control=logout" class="link-dark rounded">Sair</a></li>
           </ul>
@@ -71,8 +67,7 @@
       </li>
     </ul>
   </div>
-</main>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/sidebars.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/sidebars.js"></script>
 </body>
 </html>
